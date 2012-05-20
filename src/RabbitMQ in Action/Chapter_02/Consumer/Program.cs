7d95ac2fe299df0e0219d890214f3a31afad9196
Andrew Benz <andorbal@gmail.com>
2012-05-20 12:33:06 -0500

    Created a .NET version of the chapter 2 example from RabbitMQ in Action.

---
+++ src/RabbitMQ in Action/Chapter_02/Consumer/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Exceptions;
+
+namespace Consumer {
+  class Program {
+    static void Main(string[] args) {
+      Console.WriteLine("Starting the consumer...");
+
+      var connectionFactory = new ConnectionFactory {
+        HostName = "localhost",
+        UserName = "guest",
+        Password = "guest"
+      };
+
+      using (var connection = connectionFactory.CreateConnection()) {
+        using (var channel = connection.CreateModel()) {
+          channel.ExchangeDeclare("hello-exchange", "direct", true, false, null);
+          channel.QueueDeclare("hello-queue", true, false, false, null);
+          channel.QueueBind("hello-queue", "hello-exchange", "hola");
+
+          var consumer = new QueueingBasicConsumer(channel);
+          var consumerTag = channel.BasicConsume("hello-queue", false, consumer);
+          
+          while (true) {
+            try {
+              var e = (RabbitMQ.Client.Events.BasicDeliverEventArgs) consumer.Queue.Dequeue();
+              var bodyString = Encoding.UTF8.GetString(e.Body);
+              channel.BasicAck(e.DeliveryTag, false);
+              if (string.Equals(bodyString, "quit", StringComparison.OrdinalIgnoreCase)) {
+                channel.BasicCancel(consumerTag);
+                break;
+              }
+
+              Console.WriteLine(bodyString);
+            }
+            catch (OperationInterruptedException ex) {
+              break;
+            }
+          }
+        }
+      }
+    }
+  }
+}
---
+++ src/RabbitMQ in Action/Chapter_02/Producer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Producer {
+  class Program {
+    static void Main(string[] args) {
+      Console.WriteLine("Starting the producer...");
+
+      var connectionFactory = new ConnectionFactory {
+        HostName = "localhost",
+        UserName = "guest",
+        Password = "guest"
+      };
+
+      using (var connection = connectionFactory.CreateConnection()) {
+        using (var channel = connection.CreateModel()) {
+          channel.ExchangeDeclare("hello-exchange", "direct", true, false, null);
+
+          Console.WriteLine("Enter messages now:");
+          string message;
+          do {
+            message = Console.ReadLine();
+            var properties = channel.CreateBasicProperties();
+            properties.ContentType = "text/plain";
+            channel.BasicPublish("hello-exchange", "hola",
+                               properties, Encoding.UTF8.GetBytes(message));
+          } while (!string.Equals(message, "quit", StringComparison.OrdinalIgnoreCase));
+        }
+      }
+    }
+  }
+}
